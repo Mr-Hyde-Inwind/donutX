6005363e9d1002d202a8b1ded90a248b500ec833
Hermit <persona_gazer@outlook.com>
2026-03-29 22:14:41 +0900

    ADD: Use Vector3 to manage vector object.

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,60 @@
 const EPS = 1e-6;
 const disToScreen = 10;
 
+class Vector3 {
+    x: number;
+    y: number;
+    z: number;
+
+    static zero() {
+        return new Vector3(0, 0, 0);
+    }
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    array() {
+        return [this.x, this.y, this.z];
+    }
+
+    add(that: Vector3): Vector3 {
+        this.x += that.x;
+        this.y += that.y;
+        this.z += that.z;
+        return this;
+    }
+
+    sub(that: Vector3): Vector3 {
+        this.x -= that.x;
+        this.y -= that.y;
+        this.z -= that.z;
+        return this;
+    }
+
+    length(): number {
+        return Math.sqrt(this.x**2 + this.y**2 + this.z**2);
+    }
+
+    norm(): Vector3 {
+        const len = this.length();
+        this.x /= len;
+        this.y /= len;
+        this.z /= len;
+        return this;
+    }
+
+    dot(that: Vector3): number {
+        return this.x*that.x + this.y*that.y + this.z*that.z;
+    }
+
+    clone(): Vector3 {
+        return new Vector3(this.x, this.y, this.z);
+    }
+}
+
 function initCanvas(): CanvasRenderingContext2D {
     const screen = document.getElementById("screen") as (HTMLCanvasElement | null);
     if (screen == null) {
@@ -15,87 +69,84 @@ function initCanvas(): CanvasRenderingContext2D {
     return ctx;
 }
 
-function fillCircle(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number) {
+function fillCircle(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number, brightness: number) {
     ctx.beginPath();
+    ctx.fillStyle = `rgb(${255*brightness}, ${255*brightness}, ${255*brightness})`;
     ctx.arc(x, y, radius, 0, 2*Math.PI);
     ctx.fill();
 }
 
-function* circleIter(radius: number): Generator<[number, number]> {
+function* circleIter(radius: number): Generator<Vector3> {
     let start: number = 0;
     const thetaStep: number = Math.PI * 0.05;
     while (2*Math.PI - start > EPS) {
         let x: number = radius*Math.cos(start);
         let y: number = radius*Math.sin(start);
         start += thetaStep;
-        yield [x, y];
+        yield new Vector3(x, y, 0);
     }
 }
 
-function matrixFactor(v: Array<number>, mat: Array<Array<number>>): Array<number> {
-    let res: Array<number> = new Array<number>();
-    const cols = v.length;
-    const mRows = mat.length;
-    const mCols = mat[0]?.length;
-    if (mCols == null) {
-        throw new Error("Undefined matrix");
+function matrixFactor(v: Vector3, mat: Array<Vector3>): Vector3 {
+    if (mat.length !== 3) {
+        throw new Error("Only support 3d vector now.");
     }
-    if (mRows !== cols) {
-        throw new Error("Vector does not match with matrix");
-    }
-    for (let i = 0; i < mCols; i++) {
-        let tmp: number = 0;
-        for (let j = 0; j < mRows; j++) {
-            tmp += v[j] * mat[j][i];
-        }
-        res.push(tmp);
-    }
-    return res;
+    return new Vector3(
+        v.dot(mat[0]!),
+        v.dot(mat[1]!),
+        v.dot(mat[2]!),
+    );
 }
 
 function renderingTorus(ctx: CanvasRenderingContext2D, xAngle: number, zAngle: number) {
     const cx: number = ctx.canvas.width * 0.5;
     const cy: number = ctx.canvas.height * 0.5;
-    const r2 = 200;
+    const central: Vector3 = new Vector3(cx, cy, 0);
+    const r2: Vector3 = new Vector3(150, 0, 0);
 
     const rotateMatX = [
-        [1, 0, 0],
-        [0, Math.cos(xAngle), Math.sin(xAngle)],
-        [0, -Math.sin(xAngle), Math.cos(xAngle)]
+        new Vector3(1, 0, 0),
+        new Vector3(0, Math.cos(xAngle), -Math.sin(xAngle)),
+        new Vector3(0, Math.sin(xAngle), Math.cos(xAngle)),
     ];
 
     const rotateMatZ = [
-        [Math.cos(zAngle), Math.sin(zAngle), 0],
-        [-Math.sin(zAngle), Math.cos(zAngle), 0],
-        [0, 0, 1],
+        new Vector3(Math.cos(zAngle), -Math.sin(zAngle), 0),
+        new Vector3(Math.sin(zAngle), Math.cos(zAngle), 0),
+        new Vector3(0, 0, 1),
     ];
 
-    ctx.fillStyle = "white";
     let r1 = 100;
     while (r1 <= 100) {
         let start = 0;
         while (2*Math.PI - start > EPS) {
-            for (const [dx, dy] of circleIter(r1)) {
-                let tx = r2 + dx
-                let ty = dy;
+            for (let p of circleIter(r1)) {
+                let np: Vector3 = p.clone().norm();
+                p.add(r2);
                 const rotateMat = [
-                    [Math.cos(start), 0, Math.sin(start)],
-                    [0, 1, 0],
-                    [-Math.sin(start), 0, Math.cos(start)]
+                    new Vector3(Math.cos(start), 0, -Math.sin(start)),
+                    new Vector3(0, 1, 0),
+                    new Vector3(Math.sin(start), 0, Math.cos(start)),
                 ];
-                const [ryx, ryy, ryz] = matrixFactor([tx, ty, 0], rotateMat);
-                const [rxx, rxy, rxz] = matrixFactor([ryx, ryy, ryz], rotateMatX);
-                const [rzx, rzy, rzz] = matrixFactor([rxx, rxy, rxz], rotateMatZ);
+                p = matrixFactor(p, rotateMat);
+                p = matrixFactor(p, rotateMatX);
+                p = matrixFactor(p, rotateMatZ);
+                np = matrixFactor(np, rotateMat);
+                np = matrixFactor(np, rotateMatX);
+                np = matrixFactor(np, rotateMatZ);
+                const rp = p.clone().add(central);
 
-                const x = rzx + cx;
-                const y = rzy + cy;
-                fillCircle(ctx, x, y, 1);
+                const light: Vector3 = new Vector3(-1, 0, 1).norm();
+                const L = np.dot(light);
+                // const L = (np.dot(light) + 1) * 0.5;
+                if (L > 0) {
+                    fillCircle(ctx, rp.x, rp.y, 2.5, L);
+                }
             }
             start += Math.PI * 0.05;
         }
         r1 += 10;
     }
-
 }
 
 (() => {
@@ -111,10 +162,10 @@ function renderingTorus(ctx: CanvasRenderingContext2D, xAngle: number, zAngle: n
         ctx.fillRect(0, 0, screen.width, screen.height);
         if (start === undefined) start = timestamp;
         if (previous === undefined) previous = timestamp;
-        const delta: number = (timestamp - previous) / 1000;
+        const deltaTime: number = (timestamp - previous) / 1000;
         previous = timestamp;
-        zAngle += Math.PI * 0.2 * delta;
-        xAngle += Math.PI * 0.4 * delta;
+        zAngle += Math.PI * 0.2 * deltaTime;
+        xAngle += Math.PI * 0.4 * deltaTime;
         if (zAngle >= Math.PI * 2) zAngle -= Math.PI*2;
         if (xAngle >= Math.PI * 2) xAngle -= Math.PI*2;
         renderingTorus(ctx, xAngle, zAngle);
